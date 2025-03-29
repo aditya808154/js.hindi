e7921a84ea6ba0ad1ad5c3341230e7e9381114dc
Nageshwar Prajapati <nageshwarprajapati787@gmail.com>
2025-03-29 05:12:58 +0000

    DataType

---
+++ Basics/02_DataType.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as newer version
+//alert (3+3); // we are using nodejs, not browser
+
+
+console.log(3
+    +
+    3
+); // code readability should be high
+
+/**DataType:-primitive data types
+ * number=>2 to power 53
+ * bigint
+ * String=> "".''.``
+ * boolean==>true/fales
+ * null=>standalone value
+ * undefined=>when do not deffine value
+ * symbol=>unique
+ */
+
+console.log(typeof undefined);//undefined
+console.log(typeof null);//null
+
