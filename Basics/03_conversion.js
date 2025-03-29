29dfc52d2d960b99bbd55972fcfb453e37e9dd15
Nageshwar Prajapati <nageshwarprajapati787@gmail.com>
2025-03-29 05:46:05 +0000

    conversion

---
+++ Basics/03_conversion.js
@@ -0,0 +1,33 @@
+let score="33ss";
+// console.log(typeof score);//String 
+// console.log(typeof score);//string
+
+let valueNumber=Number(score);
+// console.log(typeof valueNumber);//number
+// console.log(valueNumber);//NaN
+
+let num=null;
+// console.log(num);//null
+let num1=Number(num);
+// console.log(num1);//0
+
+/**
+ * "33"=>33
+ * "33sds"=>NaN
+ * null=>0
+ * true=>1 false=>0
+ * ""=>false
+ * "Aditya"=>true
+ */
+
+// let isLoggedIn=1;
+// let boolenValue=Boolean(isLoggedIn);
+// console.log(boolenValue);//true
+
+
+let someNumber=33;
+let stringValue=String(someNumber);
+console.log(stringValue);//33
+console.log(typeof stringValue);//string
+
+
