319de08cf010d7d99db8b7528db92eb1839b620b
Nageshwar Prajapati <nageshwarprajapati787@gmail.com>
2025-03-29 07:01:54 +0000

    DataType

---
+++ Basics/04_Operations.js
@@ -0,0 +1,23 @@
+// console.log(20+10);//30
+// console.log(20-10);//10
+// console.log(20*10);//200
+// console.log(20**10);//102400000000
+// console.log(20/10);2
+// console.log(20%10);//0
+
+// console.log("1"+2);//12
+// console.log(1+"2");//12
+// console.log("1"+"2");12
+// console.log("1"+2+2);//122
+// console.log(1+2+"2");//32
+// console.log(1+"2"+2);//122
+
+// console.log(true);//true
+// console.log(+true);//1
+
+
+// let num=5;
+// console.log(num++);//5:-first print value and then increase value.
+// console.log(++num);//6:-first increase value and then print value.
+
+
---
+++ Basics/05_comparision.js
@@ -0,0 +1,16 @@
+// console.log(20<10);//f
+// console.log(20>10);//t
+// console.log(20<=10);//f
+// console.log(20>=10);//t
+// console.log(20==10);//f
+// console.log(20!==10);//t
+
+// console.log(null>0);//f
+// console.log(null==0);//f
+// console.log(null>=0);//t
+
+
+console.log("2"==2);//t:check without datatype
+console.log("2"===2);//f:-check with datatype
+
+
